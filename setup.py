116d64030e0967d08a6ff51722fc3e5bd8e1e50b
Liu Haijiang <liuhaijiang@saicmotor.com>
2025-07-16 12:04:25 +0800

    application

--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
-setup(
-    name="zoneclient",
-    version="0.1.0",
-    description="A short description of your package",
-    long_description=open("README.md", "r", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    author="Liu Haijiang",
-    author_email="liuhaijiang@saicmotor.com",
-    url="https://github.com/stefanzweig/dumb",
-    packages=find_packages(exclude=["tests*"]),
-    install_requires=[
-        "PyYAML==6.0.2",
-    ],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    python_requires=">=3.8",
-)
-
-
-
 #!/usr/bin/env python
 
 from os.path import abspath, dirname, join
@@ -57,12 +30,8 @@ Programming Language :: Python :: 3.13
 Programming Language :: Python :: 3.14
 Framework :: Zone Framework
 """.strip().splitlines()
-DESCRIPTION = (
-    "Zone framework for acceptance testing "
-)
-KEYWORDS = (
-    "framework automation testautomation testing atdd bdd"
-)
+DESCRIPTION = "Zone framework for acceptance testing "
+KEYWORDS = "framework automation testautomation testing atdd bdd"
 PACKAGE_DATA = [
     join("htmldata", directory, pattern)
     for directory in ("rebot", "libdoc", "testdoc", "lib", "common")
@@ -91,8 +60,5 @@ setup(
     package_dir={"": "src"},
     package_data={"zoneclient": PACKAGE_DATA},
     packages=find_packages("src"),
-    entry_points={
-        "console_scripts": [
-        ]
-    },
+    entry_points={"console_scripts": []},
 )
