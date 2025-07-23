1538ce754202dd0ff15ecaa1f97d0831d5e461a3
Chen Jiao 陈姣-ZX <chenjiao02@saicmotor.com>
2025-07-23 15:40:16 +0800

    modify docs/conf.py and reference.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import sys, os
+from sphinx.ext.autodoc import PropertyDocumenter
 
 sys.path.insert(0, os.path.abspath("../../"))
 
@@ -38,4 +39,24 @@ autodoc_default_options = {
     "members": True,
     "special-members": "__init__",
     "undoc-members": True,
+    'property-no-value': True,
+    'hide-property-decorators': True
 }
+
+
+# 方法1：修改PropertyDocumenter的显示方式
+def skip_property_header(self, sig):
+    # 完全跳过property的header生成
+    pass
+
+
+def setup(app):
+    # 方法1：直接修改PropertyDocumenter的行为
+    PropertyDocumenter.add_directive_header = skip_property_header
+
+    # 方法2：同时添加事件处理（双保险）
+    app.connect('autodoc-process-signature',
+                lambda app, what, name, obj, options, sig, ret: (None, ret) if isinstance(obj, property) else (
+                sig, ret))
+
+    return {'version': '1.0'}  # 必须返回字典
--- docs/source/reference.rst
+++ docs/source/reference.rst
@@ -7,26 +7,11 @@
 .. contents:: :local:
    :class: this-will-duplicate-information-and-it-is-still-useful-here
 
-.. currentmodule:: Clients.CanStackClient.CanStackClient
+.. currentmodule:: clients.CanStackClient.CanStackClient
 
-CANStackClient
-==============
 
-.. autoclass:: CANStackClient
-   :members:
-
-.. currentmodule:: Clients.CanParserClient.CanParserClient
-
-CanParserClient
+zone.App
 ===============
 
-.. autoclass:: CanParserClient
-   :members:
-
-.. currentmodule:: Clients.ConfigClient.ConfigClient
-
-ConfigClient
-============
-
-.. autoclass:: ConfigClient
+.. autoclass:: zone.application.App
    :members:
